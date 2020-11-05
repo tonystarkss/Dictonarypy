cfbf601094375b1e035c4c6b584e18b12e4dd1f9
tonystarkss <73152142+tonystarkss@users.noreply.github.com>
2020-11-05 11:52:01 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+# Dictionary created with a key and value for each month
+
+print(monthConversions.get("Ran", "Not a valid Month"))
+# Finds the key and prints its value
